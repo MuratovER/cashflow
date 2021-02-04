9359313b988999dd5801f6b622f502ca53891a56
MuratovER <cooperative.entr@gmail.com>
2021-02-04 17:33:36 +0545

    Added extended templates

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.shortcuts import render
 from . import views
+from django.conf.urls import include
 
 urlpatterns = [
-    
+    path('', views.home_page, name='home_page'),    
 ]
