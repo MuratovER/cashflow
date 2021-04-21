d2c9ee8cdc397cb62a2c47826181c7ab8bf42ab9
MuratovER <cooperative.entr@gmail.com>
2021-04-21 20:48:46 +0300

    Adapted business logic to the project

--- mainsite/services/services.py
+++ mainsite/services/services.py
@@ -1,7 +1,54 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
-from mainsite.forms import SignUpForm
+from django.shortcuts import render, redirect
+from django.template import RequestContext
+from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
-from django.template import RequestContext
-from django.template import RequestContext
+
+from .. import models
+
+
+def _form_save(form, redirect_url: str):
+    """
+    Функция сохраняет форму и принимает в значениях ссылку для редиректа и саму форму
+    """
+    form = form
+    if form.is_valid():
+        form.save()
+        return redirect(redirect_url)
+
+
+def _user_creations_form_save(form, request_call):
+    if form.is_valid():
+        user = form.save()
+        user.refresh_from_db()
+        # load the profile instance created by the signal
+        user.save()
+        raw_password = form.cleaned_data.get('password1')
+        # login user after signing up
+        user = authenticate(username=user.username, password=raw_password)
+        login(request_call, user)
+
+
+
+
+def _asset_count(assets) -> None:
+    """
+        Подсчитывает процент изменения цены и полную стоимость всех активов данного типа
+        Получает значение актива из таблицы asset
+    """
+    def fixed(number, digits):
+        return f"{number:.{digits}f}"
+
+    for asset in assets:
+        buy_price = asset.buy_price
+        modified_buy_price = asset.modified_buy_price
+        asset.cost_before = buy_price*asset.count
+
+        if asset.modified_buy_price:
+            asset.cost_after = modified_buy_price*asset.count
+        else:
+            asset.cost_after = asset.cost_before
+
+        if asset.modified_buy_price:
+            growth_price = ((modified_buy_price-buy_price)/buy_price)*100
+            asset.growth = fixed(growth_price, 2)
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,86 +1,50 @@
 from django.shortcuts import render, redirect
-from mainsite.forms import SignUpForm, AssetForm
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
 from django.views.decorators.csrf import csrf_exempt
-from .models import Asset, Profile
-from .forms import AssetMathForm
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from .models import Asset, Profile
+
+from .forms import AssetMathForm
+
+from mainsite.forms import SignUpForm, AssetForm
+
+from .services.services import _asset_count,\
+                               _form_save, \
+                               _user_creations_form_save
 
 
 @login_required
 def home_page(request):
-    return render(request, 'mainsite/main_page/home_page.html', )
+    return render(request, 'mainsite/main_page/home_page.html' )
 
 
 @login_required
 def asset_list(request):
-    
-    def fixed(number, digits):
-        return f"{number:.{digits}f}"  
+    assets = Asset.objects.all().order_by('asset_name')
+    _asset_count(assets)
+    context = {'assets': assets}
+    return render(request, 'mainsite/input/asset_list.html', context)
 
-    assets = Asset.objects.all().order_by('asset_name') 
-
-    for asset in assets:
-        buy_price = asset.buy_price
-        modified_buy_price = asset.modified_buy_price
-        asset.cost_before = buy_price*asset.count
-
-        if asset.modified_buy_price:
-            asset.cost_after = modified_buy_price*asset.count
-        else:
-            asset.cost_after = asset.cost_before
-
-        if asset.modified_buy_price:
-            growth_price = ((modified_buy_price-buy_price)/buy_price)*100
-            asset.growth = fixed(growth_price, 2)
-
-    
-    context = {'assets': assets} 
-    return render(request, 'mainsite/input/asset_list.html', context )
-
-def asset_counts(request):
-    asset = Asset.objects.all()
-    
-
-    context = {'growth_percantage': growth_percantage}
-
-    print('growth_percantage')
-    return render(request, 'mainsite/input/asset_list.html',context)
 
 @login_required
 def new_asset_page(request):
 
-    
     if request.method == 'POST':
-        form = AssetForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('asset_list')
+        _form_save(AssetForm(request.POST), 'asset_list')
 
-    form = AssetForm
+    form = AssetForm(request.POST)
     context = {'form': form}
-    return render(request, 'mainsite/input/new_asset.html',context)
-
+    return render(request, 'mainsite/input/new_asset.html', context)
 
 
 def signup(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()
-            # load the profile instance created by the signal
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-
-            # login user after signing up
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-
-            # redirect user to home page
-            return redirect('home_page')
+        _user_creations_form_save(SignUpForm(request.POST), request)
+        return redirect('home_page')
     else:
         form = SignUpForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        return render(request, 'registration/signup.html', {'form': form})
